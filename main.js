8e235a76694ac1841d12a9a48a20931957056555
Zachary-bungei <160045464+Zachary-bungei@users.noreply.github.com>
2024-06-17 11:41:36 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+window.onload = function() {
+  if (!sessionStorage.getItem("urlCode")) {
+    const randomCode = Math.floor(Math.random() * 10000); // Generate a random 5-digit code
+    sessionStorage.setItem("urlCode", randomCode); // Store the code in sessionStorage
+    window.history.replaceState(null, null, "/page?code=" + randomCode); // Update the URL with the new code
+  }
+};
+
 //calculte the addition
 
 function  calculate_add() {
